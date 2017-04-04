d82c7ead4fe180a991c22ec4d4843edc8999cea2
Chris Wood <woodcr5@gmail.com>
2017-04-04 17:43:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('div.target')
+}
+
+function increaseRankBy(n) {
+const rank =  document.getElementById('app').querySelectorAll('ul.ranked-list li')
+
+  for (let i = 0, l = rank.length; i < l; i++) {
+    rank[i].innerHTML = (parseInt(rank[i].innerHTML) + n).toString()
+  }
+}
+
+function deepestChild() {
+const node = document.getElementById('grand-node').querySelectorAll('div')
+
+  for (let i = 0, l = node.length; i < l; i++) {
+    if (node[i].innerHTML != null) {
+      return node[i]
+      }
+    }
+  }
